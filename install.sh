41b7ea927bdc4bd73defe7841b4d4f5873924407
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-02 22:35:19 -0800

    added basic installer routine

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+modsdir=$(pwd)/modules
+initdir=$(pwd)/fdsa
 
+[ -d $modsdir ] && [ -d $initdir ] || exit 1
+
+for module in $(ls $modsdir); do
+    while ([ ! "$answer" = "y" ] && [ ! "$answer" = "n" ]) ; do
+        echo -n "install $module? y/n "
+        read answer
+    done
+    [ "$answer" = "y" ] && ln -sf ${modsdir}/${module} ${initdir}/
+    unset answer
+done
