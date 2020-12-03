f5e3d341d56320d5a0a5f2c012ac5eca58249a6c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-02 21:37:51 -0800

    deleted unused lines

--- modules/99-startup.zsh
+++ modules/99-startup.zsh
@@ -1,6 +1,1 @@
-# neofetch --ascii_distro MacOS_small
-# neofetch --ascii_distro Linux
-# neofetch --ascii_colors 8 3 7 --source $SARBS/ascii/templeos_big
-# neofetch --ascii_colors 8 3 7 --source $SARBS/ascii/templeos
-
-lfcd
+# put programs you want to run in every new shell in here
