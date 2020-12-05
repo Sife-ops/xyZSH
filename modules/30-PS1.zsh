870eeffef2a0de92d10e176d68cad0685ff76724
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-05 04:47:02 -0800

    split prompt module into separate files

--- modules/30-PS1.zsh
+++
@@ -1,4 +0,0 @@
-# PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
-PS1="%B%{$fg[magenta]%}%(4~|.../%3~|%~) %{$fg[blue]%}>%{$reset_color%}%b "
-prompt adam2 8bit
-
---
+++ modules/30-prompt-simple.zsh
@@ -0,0 +1,1 @@
+PS1="%B%{$fg[magenta]%}%(4~|.../%3~|%~) %{$fg[blue]%}>%{$reset_color%}%b "
