594dc40a2399c8e0aed43a59d810e1a1f58c9f2b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-05 21:38:42 -0800

    added tmuxmenu-run function

--- modules/40-functions.zsh
+++ modules/40-functions.zsh
@@ -1,18 +1,27 @@
-# Copies the pathname of the current directory to the system or X Windows clipboard
 function copydir {
+    # Copies the pathname of the current directory to the system or X Windows clipboard
     emulate -L zsh
     print -n $PWD | xclip -selection clipboard
 }
 
-# Copies the contents of a given file to the system or X Windows clipboard
 function copyfile {
+    # Copies the contents of a given file to the system or X Windows clipboard
     emulate -L zsh
     cat $1 | xclip -selection clipboard
 }
 
-# copy the active line from the command line buffer onto the system clipboard
 function copybuffer () {
+    # copy the active line from the command line buffer onto the system clipboard
     printf "%s" "$BUFFER" | xclip -selection clipboard
 }
-# zle -N copybuffer
-# bindkey "^O" copybuffer
+zle -N copybuffer
+
+function tmuxmenu-run {
+    # run tmuxmenu.sh
+    if [ -z $TMUX ]; then
+        tmuxmenu.sh
+    else
+        tmux split-window -c "#{pane_current_path}" -f -v tmuxmenu.sh
+    fi
+}
+zle -N tmuxmenu-run
