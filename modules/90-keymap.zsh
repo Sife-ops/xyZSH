bf2c73c99c34e54e3a2f4bb076a1b19103c8f783
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-05 21:41:42 -0800

    added tmuxmenu-run map

--- modules/90-keymap.zsh
+++ modules/90-keymap.zsh
@@ -16,12 +16,10 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 
-# fix backspace key
-bindkey -v '^?' backward-delete-char
-export KEYTIMEOUT=1
-
 # misc.
-bindkey -M vicmd v edit-command-line
+bindkey ' ' magic-space
 bindkey '^A' autosuggest-accept
 bindkey '^Z' fancy-ctrl-z
-bindkey ' ' magic-space
+bindkey -M vicmd v edit-command-line
+isfunction copybuffer && bindkey "^o" copybuffer
+isfunction tmuxmenu-run && bindkey '^x' tmuxmenu-run
