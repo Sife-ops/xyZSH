4bc72eb8d7645c76e4f1aaa1ac2aef5c244d1998
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-08 08:07:07 -0800

    added funcy-ctrl-z

--- modules/40-functions.zsh
+++ modules/40-functions.zsh
@@ -1,27 +1,28 @@
-function copydir {
+function copydir() {
     # Copies the pathname of the current directory to the system or X Windows clipboard
     emulate -L zsh
     print -n $PWD | xclip -selection clipboard
 }
 
-function copyfile {
+function copyfile() {
     # Copies the contents of a given file to the system or X Windows clipboard
     emulate -L zsh
     cat $1 | xclip -selection clipboard
 }
 
-function copybuffer () {
+function copybuffer() {
     # copy the active line from the command line buffer onto the system clipboard
     printf "%s" "$BUFFER" | xclip -selection clipboard
 }
 zle -N copybuffer
 
-function tmuxmenu-run {
-    # run tmuxmenu.sh
-    if [ -z $TMUX ]; then
-        tmuxmenu.sh
-    else
-        tmux split-window -c "#{pane_current_path}" -f -v tmuxmenu.sh
-    fi
+function fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line -w
+  else
+    zle push-input -w
+    zle clear-screen -w
+  fi
 }
-zle -N tmuxmenu-run
+zle -N fancy-ctrl-z
