0f668572f8cc77fdef69e461d8d6dc808e549509
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-08 08:28:19 -0800

    deleted fancy-ctrl-z

--- modules/40-functions.zsh
+++ modules/40-functions.zsh
@@ -15,14 +15,3 @@ function copybuffer() {
     printf "%s" "$BUFFER" | xclip -selection clipboard
 }
 zle -N copybuffer
-
-function fancy-ctrl-z () {
-  if [[ $#BUFFER -eq 0 ]]; then
-    BUFFER="fg"
-    zle accept-line -w
-  else
-    zle push-input -w
-    zle clear-screen -w
-  fi
-}
-zle -N fancy-ctrl-z
--- modules/90-keymap.zsh
+++ modules/90-keymap.zsh
@@ -13,12 +13,11 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 
+# custom funcitons
+isfunction copybuffer && bindkey "^o" copybuffer
+
 # misc.
 bindkey -v '^?' backward-delete-char
 bindkey ' ' magic-space
 bindkey '^A' autosuggest-accept
 bindkey -M vicmd v edit-command-line
-
-# custom funcitons
-isfunction copybuffer && bindkey "^o" copybuffer
-isfunction fancy-ctrl-z && bindkey '^Z' fancy-ctrl-z
