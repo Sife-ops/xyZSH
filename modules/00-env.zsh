8f477a46b90d2084a2951f81cc1bbdbf59568a4a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-01-31 07:54:43 -0800

    removed unnecessary modules

--- modules/00-env.zsh
+++
@@ -1,4 +0,0 @@
-export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/history"
-export HISTSIZE=10000
-export SAVEHIST=9000
-
--- modules/99-startup.zsh
+++
@@ -1,1 +0,0 @@
-# put programs you want to run in every new shell in here
