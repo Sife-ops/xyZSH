3611d673f4b99226425f3b08da19231ce26372e1
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-02 07:32:40 -0800

    removed unnecessary modules

--- modules/60-files.zsh
+++
@@ -1,8 +0,0 @@
-alias cfrf="$EDITOR ~/.config/zsh/conf.d/files.zsh"
-alias cfdy="$EDITOR ~/.config/zsh/conf.d/directories.zsh"
-alias cfv="$EDITOR ~/.config/nvim/init.vim"
-alias cfa="$EDITOR ~/.config/aliasrc"
-alias cfz="$EDITOR ~/.config/zsh/.zshrc"
-alias rfb="$EDITOR ~/Documents/bookmarks"
-
-
