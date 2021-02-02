7c84c0c99ae6403e43173ee1d70bd1dfaf125bec
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-02 01:44:56 -0800

    added shortcuts file

---
+++ modules/60-shortcuts.zsh
@@ -0,0 +1,19 @@
+hash -d bin="$HOME/.local/bin"
+hash -d config="${XDG_CONFIG_HOME:-$HOME/.config}"
+hash -d doc="${XDG_DATA_HOME:-$HOME/.local/share}/doc"
+hash -d dv="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+hash -d dz="${XDG_CONFIG_HOME:-$HOME/.config}/zsh"
+hash -d profiles="${XDG_DATA_HOME:-$HOME/.local/share}/sarbs/profiles"
+hash -d sarbs="${XDG_DATA_HOME:-$HOME/.local/share}/sarbs"
+hash -d share="${XDG_DATA_HOME:-$HOME/.local/share}"
+hash -d src="$HOME/.local/src"
+
+alias bin='cd ~bin'
+alias config='cd ~config'
+alias doc='cd ~doc'
+alias dv='cd ~dv'
+alias dz='cd ~dz'
+alias profiles='cd ~profiles'
+alias sarbs='cd ~sarbs'
+alias share='cd ~share'
+alias src='cd ~src'
