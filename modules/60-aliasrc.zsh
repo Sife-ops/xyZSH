67ba583b8785b311954876842d0d03e1412cf76b
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-01 21:37:05 -0800

    updated aliases

--- modules/60-aliasrc.zsh
+++ modules/60-aliasrc.zsh
@@ -1,8 +1,10 @@
-#
-# Sife's .aliasrc
-#
+#            _ _
+#       __ _| (_) __ _ ___ _ __ ___
+#      / _` | | |/ _` / __| '__/ __|
+#     | (_| | | | (_| \__ \ | | (__
+#    (_)__,_|_|_|\__,_|___/_|  \___|
 
-# #--- COMMAND OPTIONS ---# #
+#^----- COMMAND OPTIONS --------------------------------------------------------
 
 alias \
     cp="cp -iv" \
@@ -16,20 +18,27 @@ alias \
     mkdir="mkdir -pv" \
     rm="rm -vI"
 
-# #--- ABBREVIATIONS ---# #
+#$
 
-# General
+#^----- ABBREVIATIONS ----------------------------------------------------------
+
+#^ General
+
 alias \
+    abook="abook -C ${XDG_CONFIG_HOME:-${HOME}/.config}/abook/abookrc \\
+        --datafile ${XDG_DATA_HOME:-${HOME}/.local/share}/addressbook" \
     cal='calcurse' \
     chx='chmod +x' \
     d='dragon-drag-and-drop' \
     f='lfcd' \
     fcl='fc-list : family style | less' \
     fvm='sudo fuser -vm' \
-    irssi="irssi --home=${XDG_CONFIG_HOME:-$HOME/.config}/irssi" \
+    irssi="irssi --config=${XDG_CONFIG_HOME:-${HOME}/.config}/irssi/config \\
+        --home=${XDG_CONFIG_HOME:-${HOME}/.config}/irssi" \
     k='kill' \
     ka="killall" \
     ll='ls -la --color=auto' \
+    myip='dig +short myip.opendns.com @resolver1.opendns.com' \
     n='nnncd' \
     nb='newsboat' \
     offlineimap="offlineimap -c \\
@@ -38,18 +47,33 @@ alias \
     pg='pgrep' \
     pk='pkill' \
     r='rangercd' \
+    rtorrent='rtorrent -n -o import=~/.config/rtorrent/rtorrent.rc' \
 	sdn="sudo shutdown -h now" \
     so="source {${XDG_CONFIG_HOME:-${HOME}/.config}/aliasrc,${ZDOTDIR}/.zshrc}" \
+    so="source ~/.zprofile; source ${ZDOTDIR}/.zshrc" \
     tp='trash-put' \
     xo='xdg-open' \
     xr='xrdb ~/.Xresources' \
     z="zathura"
 
-# Git
+#$
+
+#^ Emacs
+
+systemctl --quiet --user is-active emacs.service >/dev/null \
+    && alias e='emacsclient' \
+    || alias e='emacs'
+
+#$
+
+#^ Git
+
 alias \
     ga='git add' \
+    gap='git add -p' \
     gau='git add -u' \
-    gch='git checkout --' \
+    gb='git branch' \
+    gch='git checkout' \
     gcm='git commit -m' \
     gdh='git diff HEAD | less' \
     gi="git status --porcelain | grep '^??' | cut -c4- >> .gitignore" \
@@ -60,22 +84,38 @@ alias \
     grh='git reset --hard' \
     grl='git rev-list --pretty=oneline --abbrev-commit HEAD | less' \
     grm='git rm -r --cached' \
-    gs='git status' 
+    gs='git status'
 
-# Emacs
-systemctl --quiet --user is-active emacs.service >/dev/null \
-    && alias e='emacsclient' \
-    || alias e='emacs'
+#$
 
-# Vim
-command -v nvim >/dev/null \
-    && alias vim="nvim" v="nvim" vimdiff="nvim -d"  \
-    || alias v="vim"
+#^ Mutt
 
-# Tmux
+command -v neomutt >/dev/null \
+    && alias m="cd ~/Downloads && neomutt" mutt="cd ~/Downloads && neomutt" \
+    || alias m="cd ~/Downloads && mutt" mutt="cd ~/Downloads && mutt"
+
+#$
+
+#^ Tmux
+
 alias \
 	t="tmux -f ${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf" \
     ta='tmux attach-session -t' \
+    tg='tmux new-session -t' \
     tk='tmux kill-session -t' \
     tl='tmux list-sessions' \
 	tmux="tmux -f ${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf"
+
+#$
+
+#^ Vim
+
+command -v nvim >/dev/null \
+    && alias vim="nvim" v="nvim" vimdiff="nvim -d" vd="nvim -d"  \
+    || alias v="vim" vimdiff="vim -d" vd="vim -d"
+
+#$
+
+#$
+
+# vim: fdm=marker fmr=#^,#$
