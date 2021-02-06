aecaae2688803fd309f779a3be72b3d7f175d9e6
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-02-06 11:17:47 -0800

    fixed reverse option

--- modules/50-fzf.zsh
+++ modules/50-fzf.zsh
@@ -2,7 +2,8 @@ source /usr/share/fzf/completion.zsh 2>/dev/null # Arch
 source /usr/share/fzf/key-bindings.zsh 2>/dev/null # Arch
 source /usr/share/doc/fzf/examples/key-bindings.zsh 2>/dev/null # Debian
 
+export FZF_CTRL_R_OPTS="--reverse"
+
 if [ -d /var/lib/relocate ]; then
     export FZF_ALT_C_COMMAND="cat /var/lib/relocate/relocated.db"
-    export FZF_CTRL_R_OPTS="--reverse"
 fi
